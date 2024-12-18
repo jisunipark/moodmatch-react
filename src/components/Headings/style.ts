a19f77e009741c7d6a367262cd8dc710eb0d4965
Jisun Park <jisunpark.t@gmail.com>
2024-12-18 14:50:32 +0900

    design: h1 - h6 스타일 추가

---
+++ src/components/Headings/style.ts
@@ -0,0 +1,66 @@
+import styled from '@emotion/styled';
+import { darkText } from '../../styles/colors';
+
+export const H1 = styled.h1`
+  color: ${darkText};
+  text-align: center;
+  font-family: 'Plus Jakarta Sans';
+  font-size: 120px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120px; /* 100% */
+  letter-spacing: -2.4px;
+`;
+
+export const H2 = styled.h2`
+  color: ${darkText};
+  text-align: center;
+  font-family: 'Plus Jakarta Sans';
+  font-size: 112px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120px; /* 107.143% */
+  letter-spacing: -2.8px;
+`;
+
+export const H3 = styled.h3`
+  color: ${darkText};
+  text-align: center;
+  font-family: 'Plus Jakarta Sans';
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 68px; /* 106.25% */
+  letter-spacing: -1.28px;
+`;
+
+export const H4 = styled.h4`
+  color: ${darkText};
+  font-family: 'Plus Jakarta Sans';
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 52px; /* 108.333% */
+  letter-spacing: -1.2px;
+`;
+
+export const H5 = styled.h5`
+  color: ${darkText};
+  text-align: center;
+  font-family: 'Plus Jakarta Sans';
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 32px; /* 114.286% */
+  letter-spacing: -0.56px;
+`;
+
+export const H6 = styled.h6`
+  color: ${darkText};
+  font-family: 'Plus Jakarta Sans';
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 32px; /* 114.286% */
+  letter-spacing: -0.7px;
+`;
