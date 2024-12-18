39736e896c206a30a48d8de417f2652b22b5b289
Jisun Park <jisunpark.t@gmail.com>
2024-12-18 15:06:03 +0900

    design: 페이지 공통 스타일 추가

---
+++ src/styles/page.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import { darkBg } from './colors';
+
+export const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 60px auto 0;
+  background-color: ${darkBg};
+`;
