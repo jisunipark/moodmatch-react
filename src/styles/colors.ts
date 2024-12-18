6bfba73ce14568f5cc8f8ce6afe75e9d4483403a
Jisun Park <jisunpark.t@gmail.com>
2024-12-18 14:39:28 +0900

    design: 색상값 변수 지정

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const primary = '#F2441E';
+
+export const darkBg = '#0D0402';
+export const darkText = '#E4DAD7';
+
+export const lightBg = '#FFFBFA';
+export const lightText = '#281D1B';
+
+export const buttonText = '#030100';
