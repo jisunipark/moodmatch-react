e0dd9fc89055890682f9d8821a60730fb8059fe7
Jisun Park <jisunpark.t@gmail.com>
2024-12-19 21:06:20 +0900

    feat: 로고 클릭 시 첫 페이지로 이동

--- src/components/Gnb/Logo/Logo.tsx
+++ src/components/Gnb/Logo/Logo.tsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import LogoIcon from '../../Icons/LogoIcon';
 import * as S from './style';
 
 export default function Logo() {
   return (
-    <S.Wrapper>
-      <LogoIcon />
-      <S.LogoText>MoodMatch</S.LogoText>
-    </S.Wrapper>
+    <Link to="/">
+      <S.Wrapper>
+        <LogoIcon />
+        <S.LogoText>MoodMatch</S.LogoText>
+      </S.Wrapper>
+    </Link>
   );
 }
